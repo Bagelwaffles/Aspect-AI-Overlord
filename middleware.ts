92a50a88e89fe59fcb12b0cc8f619f172a7586ce
Kimberley Avers <31176399+Bagelwaffles@users.noreply.github.com>
2026-02-26 20:09:58 -0600

    Add '/agents' route to middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/dashboard/:path*", "/profile/:path*"],
+      matcher: [
+              "/dashboard/:path*",
+              "/profile/:path*",
+              "/agents/:path*",
+            ],
 };
